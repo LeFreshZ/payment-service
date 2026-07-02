3d48ed45a83ff88c70d8f9f5b59a9a4b6cb53aad
lefreshz <scver1@yandex.ru>
2026-07-02 11:12:00 +0300

    Add DTO, mapper, client for accessing API for random numbers and service layer

---
+++ src/main/java/com/innowise/paymentservice/service/implementation/PaymentServiceImpl.java
@@ -0,0 +1,82 @@
+package com.innowise.paymentservice.service.implementation;
+
+import com.innowise.paymentservice.dao.PaymentDao;
+import com.innowise.paymentservice.dto.CreatePaymentRequest;
+import com.innowise.paymentservice.dto.PaymentResponse;
+import com.innowise.paymentservice.dto.PaymentSummaryResponse;
+import com.innowise.paymentservice.entity.Payment;
+import com.innowise.paymentservice.entity.enums.PaymentStatus;
+import com.innowise.paymentservice.exception.PaymentNotFoundException;
+import com.innowise.paymentservice.mapper.PaymentMapper;
+import com.innowise.paymentservice.service.PaymentService;
+import java.time.Instant;
+import java.util.List;
+import java.util.Optional;
+import lombok.AllArgsConstructor;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class PaymentServiceImpl implements PaymentService {
+
+  private final PaymentDao dao;
+  private final PaymentMapper mapper;
+  private final RandomNumberClient randomNumberClient;
+
+  @Override
+  public PaymentResponse createPayment(CreatePaymentRequest request, Long userId) {
+    Payment payment = mapper.toEntity(request);
+    payment.setUserId(userId);
+    payment.setStatus(PaymentStatus.PENDING);
+    payment.setTime(Instant.now());
+
+    Payment savedPayment = dao.save(payment);
+
+    int randomNumber = randomNumberClient.getRandomNumber();
+
+    PaymentStatus finalStatus = randomNumber % 2 == 0
+        ? PaymentStatus.SUCCESS
+        : PaymentStatus.FAILED;
+
+    savedPayment.setStatus(finalStatus);
+    Payment updatedPayment = dao.save(savedPayment);
+
+    return mapper.toResponse(updatedPayment);
+  }
+
+  @Override
+  public PaymentResponse getPaymentById(String id, Long userId, boolean isAdmin) {
+    Optional<Payment> optionalPayment = dao.findById(id);
+
+    if (optionalPayment.isEmpty()) {
+      throw new PaymentNotFoundException("Payment not found with id: " + id);
+    }
+
+    Payment payment = optionalPayment.get();
+
+    if (!isAdmin && !payment.getUserId().equals(userId)) {
+      throw new AccessDeniedException("Access denied");
+    }
+
+    return mapper.toResponse(payment);
+  }
+
+  @Override
+  public List<PaymentResponse> getPayments(Long userId, Long orderId, PaymentStatus status) {
+    return dao.findByFilters(userId, orderId, status)
+        .stream()
+        .map(mapper::toResponse)
+        .toList();
+  }
+
+  @Override
+  public PaymentSummaryResponse getUserPaymentSummary(Long userId, Instant from, Instant to) {
+    return new PaymentSummaryResponse(dao.sumSuccessfulPaymentsForUser(userId, from, to));
+  }
+
+  @Override
+  public PaymentSummaryResponse getAllUsersPaymentSummary(Instant from, Instant to) {
+    return new PaymentSummaryResponse(dao.sumSuccessfulPaymentsForAll(from, to));
+  }
+}
---
+++ src/main/java/com/innowise/paymentservice/service/implementation/RandomNumberClient.java
@@ -0,0 +1,47 @@
+package com.innowise.paymentservice.service.implementation;
+
+import java.util.Random;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Slf4j
+@Component
+public class RandomNumberClient {
+  private final WebClient webClient;
+
+  public RandomNumberClient(
+      WebClient.Builder builder,
+      @Value("${random-api.url}") String randomApiUrl
+  ) {
+
+    this.webClient = builder.baseUrl(randomApiUrl).build();
+  }
+
+  public int getRandomNumber() {
+    try {
+      String response = webClient.get()
+          .uri(uriBuilder -> uriBuilder
+              .queryParam("num", 1)
+              .queryParam("min", 1)
+              .queryParam("max", 1000)
+              .queryParam("col", 1)
+              .queryParam("base", 10)
+              .queryParam("format", "plain")
+              .queryParam("rnd", "new")
+              .build())
+          .retrieve()
+          .bodyToMono(String.class)
+          .block();
+
+      return Integer.parseInt(response.trim());
+    } catch (Exception ex) {
+      log.warn("Random API unavailable, using fallback: {}", ex.getMessage());
+
+      Random rnd = new Random();
+
+      return rnd.nextInt(1, 1000);
+    }
+  }
+}
